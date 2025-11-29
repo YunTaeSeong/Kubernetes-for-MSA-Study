534efcfb17dddc3367267b29e8b761bda0a3cf4c
YunTaeSeong <xoqkf1234@naver.com>
2025-11-29 20:10:55 +0900

    MSA에서 RateLimiter 설정

--- bank/accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
+++ bank/accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.bank.accounts.dto.CustomerDto;
 import com.bank.accounts.dto.ErrorResponseDto;
 import com.bank.accounts.dto.ResponseDto;
 import com.bank.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -231,11 +232,18 @@ public class AccountsController {
                     )
             ),
     })
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("JAVA 21");
     }
 
     @Operation(
